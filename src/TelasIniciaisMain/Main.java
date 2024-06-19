0f07910cc4f58237920c78613a5f15027c76858d
Henrique1004 <henrique.oliveira@socin.com.br>
2024-06-18 23:29:41 -0300

    commit trabalho final

--- src/TelasIniciaisMain/Main.java
+++ src/TelasIniciaisMain/Main.java
@@ -10,18 +10,18 @@ public class Main {
     public static void main(String[] args) {
         DBManager.createSessionFactory();
 
+//        Usuario usuario = new Usuario("Henrique", "123456", "adm");
+//        try {
+//            DBManager.getDatabaseSessionFactory().fromTransaction(session -> {
+//                session.persist(usuario);
+//                return null;
+//            });
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         SwingUtilities.invokeLater(() -> {
             TelaLogin telaLogin = new TelaLogin();
             telaLogin.setVisible(true);
         });
-        Usuario usuario = new Usuario("Henrique", "123456", "adm");
-        try {
-            DBManager.getDatabaseSessionFactory().fromTransaction(session -> {
-                session.persist(usuario);
-                return null;
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
